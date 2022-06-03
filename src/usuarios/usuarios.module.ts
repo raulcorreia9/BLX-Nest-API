8e47a5e2f5cd482e686f6ce520717484167f0b62
Raul Correia <raulvinicius245@gmail.com>
2022-06-02 22:13:25 -0300

    Integrando usuarios module com typeorm

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,8 +1,11 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { Usuarios } from './entities/usuario.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Usuarios])],
   controllers: [UsuariosController],
   providers: [UsuariosService]
 })
