4827ea0a28f35fe69f46bd7cbbf40b569b7cfdfe
Raul Correia <raulvinicius245@gmail.com>
2022-06-02 22:13:35 -0300

    Crud básico usuário.

--- src/usuarios/usuarios.service.ts
+++ src/usuarios/usuarios.service.ts
@@ -1,26 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { Usuarios } from './entities/usuario.entity';
 
 @Injectable()
 export class UsuariosService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+  constructor(
+    @InjectRepository(Usuarios)
+    private readonly usuariosRepository: Repository<Usuarios>
+  ){}
+
+  async create(createUsuarioDto: CreateUsuarioDto): Promise<Usuarios> {
+    const { email } = createUsuarioDto;
+    const busca_email = await this.usuariosRepository.findOne({ email });
+    
+    if(busca_email){
+      throw new BadRequestException('Já existe um usuário cadastrado com esse email.');
+    }
+
+    const user = await this.usuariosRepository.create({ ...createUsuarioDto });
+    return this.usuariosRepository.save(user);
   }
 
   findAll() {
-    return `This action returns all usuarios`;
+    return this.usuariosRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} usuario`;
+  async findOne(id: number): Promise<Usuarios> {
+    const usuario = await this.usuariosRepository.findOne(id);
+
+    if(!usuario) {
+      throw new NotFoundException(`Não foi encontrado usuario com ID = #${id}`);
+    }
+
+    return usuario;
   }
 
-  update(id: number, updateUsuarioDto: UpdateUsuarioDto) {
-    return `This action updates a #${id} usuario`;
+  async update(id: number, updateUsuarioDto: UpdateUsuarioDto): Promise<Usuarios> {
+    //Preload -> Se não existir usuário, cria, se existir com o ID informado, atualiza os dados
+    const user = await this.usuariosRepository.preload({
+      id: id,
+      ...updateUsuarioDto,
+    });
+
+    if(!user) {
+      throw new NotFoundException(`Não foi encontrado usuário com ID = ${id}`);
+    }
+    
+    return this.usuariosRepository.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} usuario`;
+  async remove(id: number) {
+    const usuario = await this.usuariosRepository.findOne(id);
+
+    if(!usuario) {
+      throw new NotFoundException(`Não foi encontrado usuario com ID = #${id}`);
+    }
+
+    return this.usuariosRepository.remove(usuario);
   }
 }
